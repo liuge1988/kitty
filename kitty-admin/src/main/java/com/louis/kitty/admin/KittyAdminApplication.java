dbd33f3aeac278b06eadf81809c66ef56d39d99d
Xu <503018338@logon.ds.ge.com>
2018-08-15 18:31:15 +0800

    搭建项目环境

---
+++ kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
@@ -0,0 +1,12 @@
+package com.louis.kitty.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication()
+public class KittyAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KittyAdminApplication.class, args);
+	}
+}
