36fe2ddfaa989aba9424c43f597744bd41a57b35
Xu <503018338@logon.ds.ge.com>
2018-09-26 18:36:35 +0800

    解决备份路径的问题

--- kitty-backup/src/main/java/com/louis/kitty/backup/constants/BackupConstants.java
+++ kitty-backup/src/main/java/com/louis/kitty/backup/constants/BackupConstants.java
@@ -5,7 +5,7 @@ import java.io.File;
 public interface BackupConstants {
 	
 	/** 备份目录 */
-	public static final String BACKUP_FOLDER = new File(BackupConstants.class.getClassLoader().getResource("").getPath()).getParentFile().getParentFile().getAbsolutePath() + File.separator + "_backup" + File.separator;
+	public static final String BACKUP_FOLDER = new File(BackupConstants.class.getClassLoader().getResource("").getPath()).getAbsolutePath() + File.separator + "_backup" + File.separator;
 	/** 还原目录，默认就是备份目录 */
 	public static final String RESTORE_FOLDER = BACKUP_FOLDER;
 	/** 日期格式 */
