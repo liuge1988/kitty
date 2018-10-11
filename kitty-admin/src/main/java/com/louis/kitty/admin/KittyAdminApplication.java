9c2f4b8de7fa038d2584c78afd5a1a739b9d675b
Xu <503018338@logon.ds.ge.com>
2018-10-11 15:22:23 +0800

    jar

--- kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
+++ kitty-admin/src/main/java/com/louis/kitty/admin/KittyAdminApplication.java
@@ -2,17 +2,10 @@ package com.louis.kitty.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(scanBasePackages={"com.louis.kitty"})
-public class KittyAdminApplication extends SpringBootServletInitializer {
+public class KittyAdminApplication {
 
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-		return builder.sources(KittyAdminApplication.class);
-	}
-		
 	public static void main(String[] args) {
 		SpringApplication.run(KittyAdminApplication.class, args);
 	}
