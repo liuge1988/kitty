ad36ff57b73ef913697c6e37289649e2d88f4a64
Xu <503018338@logon.ds.ge.com>
2018-10-30 17:44:29 +0800

    Spring Cloud 支持：服务注册和发现（Consul）

--- kitty-admin/src/main/java/com/louis/kitty/admin/config/ShiroConfig.java
+++ kitty-admin/src/main/java/com/louis/kitty/admin/config/ShiroConfig.java
@@ -47,6 +47,8 @@ public class ShiroConfig {
         filterMap.put("/webjars/springfox-swagger-ui/**", "anon");
         // 验证码
         filterMap.put("/captcha.jpg**", "anon");
+        // 服务监控
+        filterMap.put("/actuator/**", "anon");
         // 其他所有路径交给OAuth2Filter处理
 		filterMap.put("/**", "oauth2");
 		shiroFilter.setFilterChainDefinitionMap(filterMap);
--- kitty-monitor/src/main/java/com/louis/kitty/monitor/KittyMonitorApplication.java
+++ kitty-monitor/src/main/java/com/louis/kitty/monitor/KittyMonitorApplication.java
@@ -2,6 +2,7 @@ package com.louis.kitty.monitor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 
@@ -11,6 +12,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
  * @date Oct 29, 2018
  */
 @EnableAdminServer
+@EnableDiscoveryClient
 @SpringBootApplication
 public class KittyMonitorApplication {
 
