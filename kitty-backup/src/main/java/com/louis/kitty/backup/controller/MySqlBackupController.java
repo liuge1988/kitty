fc29e269dd9a35f762e968aa7b38165e8998461c
Xu <503018338@logon.ds.ge.com>
2018-11-04 17:49:13 +0800

    修复jar部署方式，系统数据备份还原功能不可用的问题

--- kitty-backup/src/main/java/com/louis/kitty/backup/controller/MySqlBackupController.java
+++ kitty-backup/src/main/java/com/louis/kitty/backup/controller/MySqlBackupController.java
@@ -1,6 +1,7 @@
 package com.louis.kitty.backup.controller;
 
 import java.io.File;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
@@ -37,23 +38,30 @@ public class MySqlBackupController {
 
 	@GetMapping("/backup")
 	public HttpResult backup() {
+		String backupFodlerName = BackupConstants.DEFAULT_BACKUP_NAME + "_" + (new SimpleDateFormat(BackupConstants.DATE_FORMAT)).format(new Date());
+		return backup(backupFodlerName);
+	}
+
+	private HttpResult backup(String backupFodlerName) {
 		String host = properties.getHost();
 		String userName = properties.getUserName();
 		String password = properties.getPassword();
 		String database = properties.getDatabase();
-		String backupFodlerName = BackupConstants.DEFAULT_BACKUP_NAME+ "_" + (new SimpleDateFormat(BackupConstants.DATE_FORMAT)).format(new Date());
 		String backupFolderPath = BackupConstants.BACKUP_FOLDER + backupFodlerName + File.separator;
 		String fileName = BackupConstants.BACKUP_FILE_NAME;
 		try {
-			mysqlBackupService.backup(host, userName, password, backupFolderPath, fileName, database);
+			boolean success = mysqlBackupService.backup(host, userName, password, backupFolderPath, fileName, database);
+			if(!success) {
+				HttpResult.error("数据备份失败");
+			}
 		} catch (Exception e) {
 			return HttpResult.error(500, e.getMessage());
 		}
 		return HttpResult.ok();
 	}
-
+	
 	@GetMapping("/restore")
-	public HttpResult restore(@RequestParam String name) {
+	public HttpResult restore(@RequestParam String name) throws IOException {
 		String host = properties.getHost();
 		String userName = properties.getUserName();
 		String password = properties.getPassword();
@@ -69,28 +77,35 @@ public class MySqlBackupController {
 	
 	@GetMapping("/findRecords")
 	public HttpResult findBackupRecords() {
-		List<Map<String, Object>> backupRecords = new ArrayList<>();
+		if(!new File(BackupConstants.DEFAULT_RESTORE_FILE).exists()) {
+			// 初始默认备份文件
+			backup(BackupConstants.DEFAULT_BACKUP_NAME);
+		}
+		List<Map<String, String>> backupRecords = new ArrayList<>();
 		File restoreFolderFile = new File(BackupConstants.RESTORE_FOLDER);
 		if(restoreFolderFile.exists()) {
 			for(File file:restoreFolderFile.listFiles()) {
-				Map<String, Object> bean = new HashMap<>();
-				bean.put("name", file.getName());
-				bean.put("title", file.getName());
-				if(BackupConstants.DEFAULT_BACKUP_NAME.equals(file.getName())) {
-					bean.put("title", "系统默认备份");
+				Map<String, String> backup = new HashMap<>();
+				backup.put("name", file.getName());
+				backup.put("title", file.getName());
+				if(BackupConstants.DEFAULT_BACKUP_NAME.equalsIgnoreCase(file.getName())) {
+					backup.put("title", "系统默认备份");
 				}
-				backupRecords.add(bean);
+				backupRecords.add(backup);
 			}
 		}
+		// 排序，默认备份最前，然后按时间戳排序，新备份在前面
+		backupRecords.sort((o1, o2) -> BackupConstants.DEFAULT_BACKUP_NAME.equalsIgnoreCase(o1.get("name")) ? -1
+				: BackupConstants.DEFAULT_BACKUP_NAME.equalsIgnoreCase(o2.get("name")) ? 1 : o2.get("name").compareTo(o1.get("name")));
 		return HttpResult.ok(backupRecords);
 	}
 	
 	@GetMapping("/delete")
 	public HttpResult deleteBackupRecord(@RequestParam String name) {
-		if(BackupConstants.DEFAULT_BACKUP_NAME.equals(name)) {
+		if(BackupConstants.DEFAULT_BACKUP_NAME.equals(name)) {   	
 			return HttpResult.error("系统默认备份无法删除!");
 		}
-		String restoreFilePath = BackupConstants.RESTORE_FOLDER + name;
+		String restoreFilePath = BackupConstants.BACKUP_FOLDER + name;
 		try {
 			FileUtils.deleteFile(new File(restoreFilePath));
 		} catch (Exception e) {
